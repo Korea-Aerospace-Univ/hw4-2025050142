7595bb1c72f0799f385c0a57f97d68bbcad98257
2025050142 <dongswoosh@naver.com>
2026-04-20 16:13:24 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -8,36 +8,37 @@ int main(void) {
     int cur_alpha = 0, cur_num = 0;  // 현재 연속 횟수 카운트
 
     scanf("%d", &n);  // 전체 문자의 개수 입력
-
+    
+    getchar(); 
 
     for (int i = 0; i < n; i++)  // N번만큼 반복하며 문자 하나씩 검사
-	{
-        scanf(" %c", &ch);
+    {
+        ch = getchar(); 
 
         // i) 영어 소문자
         if (ch >= 'a' && ch <= 'z') {
-            cur_alpha++;  // 영어 소문자 연속 카운트 증가
-            cur_num = 0;  // 숫자 연속은 초기화
-        }
-        
-		// ii) 숫자
-        else if (ch >= '0' && ch <= '9') {
-            cur_num++;  // 숫자 연속 카운트 증가
-            cur_alpha = 0;  // 영어 소문자 연속은 초기화
-        }
-        
-		// iii) 그 외 (영어 대문자 등)
-        else {
-            cur_alpha = 0;  // 영어 소문자 연속 카운트 초기화
+            cur_alpha++;  // 소문자 연속 증가
             cur_num = 0;  // 숫자 연속 카운트 초기화
+            
+            if (cur_alpha > max_alpha) {
+                max_alpha = cur_alpha;  // 영어 소문자 카운트가 증가했을 때 max_alpha 갱신
+            }
         }
-
-        // 문자를 하나 읽을 때마다 최고 카운트 갱신
-        if (cur_alpha > max_alpha) {
-            max_alpha = cur_alpha;
+        
+        // ii) 숫자
+        else if (ch >= '0' && ch <= '9') {
+            cur_num++;  // 숫자 연속 증가
+            cur_alpha = 0;  // 소문자 연속 카운트 초기화
+         
+            if (cur_num > max_num) {
+                max_num = cur_num;  // 숫자 카운트가 증가했을 때 max_num 갱신
+            }
         }
-        if (cur_num > max_num) {
-            max_num = cur_num;
+        
+        // iii) 그 외 (영어 대문자 등)
+        else {
+            cur_alpha = 0;  // 소문자 연속 카운트 초기화
+            cur_num = 0;    // 숫자 연속 카운트 초기화
         }
     }
 
